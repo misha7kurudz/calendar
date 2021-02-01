ec7abe352f8a2d684b9b65a764f4000cef296aa3
380668054661 <misha.kurudz77@gmail.com>
2021-02-01 13:00:40 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseService service = new DatabaseService();
+            service.RandomInsertionInto("Calendars", 5);
+            service.ShowAllDataFrom("Calendars");
+        }
+    }
+}
